5ee2167da2225a7530381556ce0763f8e9a6f316
romario <romario151996@gmail.com>
2022-08-24 21:59:29 -0500

    first commit

---
+++ src/main/java/com/nttda/nttdataservice/NttdataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttda.nttdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttdataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttdataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttda/nttdataservice/NttdataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttda.nttdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttdataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
